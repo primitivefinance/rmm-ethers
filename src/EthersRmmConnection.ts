dbacca011ad136f08029b86b074ec156742fbee7
alex <alexangel.j@gmail.com>
2022-01-06 17:53:27 -0800

    remove dev file from deployments in connection

--- src/EthersRmmConnection.ts
+++ src/EthersRmmConnection.ts
@@ -3,13 +3,11 @@ import { Signer } from '@ethersproject/abstract-signer'
 import { _connectToContracts, _RmmContractAddresses, _RmmContracts, _RmmDeploymentJSON } from './contracts'
 import { EthersProvider, EthersSigner } from './types'
 
-import dev from '../deployments/default/dev.json'
 import rinkeby from '../deployments/default/rinkeby.json'
 import goerli from '../deployments/default/goerli.json'
 import kovan from '../deployments/default/kovan.json'
 
 const deployments: { [chainId: number]: _RmmDeploymentJSON | undefined } = {
-  [dev.chainId]: dev,
   [rinkeby.chainId]: rinkeby,
   [goerli.chainId]: goerli,
   [kovan.chainId]: kovan,
